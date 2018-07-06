d0793ef4e3b402efeddd56b28dac55ba6e8501ea
jpb8vf <34172596+jpb8vf@users.noreply.github.com>
2018-07-05 20:22:39 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace URLGeneration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string project_name = " ";
+            string activity_name = " ";
+
+            try
+            {
+                Console.WriteLine("Enter the project name");
+                project_name = Console.ReadLine();
+
+
+                Console.WriteLine("Enter the activity name");
+                activity_name = Console.ReadLine();
+
+
+                string url = "https://companyserver.com/content/" + project_name + "/files/" + activity_name + "/" + activity_name + "Report.pdf";
+                Console.WriteLine("\n");
+                Console.WriteLine(url);
+                Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("PLease enter vaild details");
+            }
+
+        }
+    }
+}
+
